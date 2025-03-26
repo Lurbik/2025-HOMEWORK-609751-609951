14c32658950cac9e1c7c1606e0af4840877c2fe3
Lurbik <ludivico.toto@gmail.com>
2025-03-26 16:16:05 +0100

    junit funge juniiiiiit
    juuunittt

--- test/test/TestStanza.java
+++ test/test/TestStanza.java
@@ -1,5 +1,14 @@
 package test;
 
-public class TestStanza {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestStanza {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 
 }
