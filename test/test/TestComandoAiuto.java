795c03a0a66c4a98e793c5590b8bbb8deadd65d2
Lurbik <ludivico.toto@gmail.com>
2025-05-09 14:06:26 +0200

    sto sclerando con i test comandi

--- test/test/TestComandoAiuto.java
+++ test/test/TestComandoAiuto.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import it.uniroma3.diadia.*;
-import it.uniroma3.diadia.Simulatore;
 import it.uniroma3.diadia.comandi.ComandoAiuto;
 import it.uniroma3.diadia.comandi.ComandoFine;
 
