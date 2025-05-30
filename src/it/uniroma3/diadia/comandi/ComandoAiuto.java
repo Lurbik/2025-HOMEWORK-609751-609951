fa2b44f7d30a6da15e59a80d8044e25ab6b91b06
samuuu17 <samuelemiele0@gmail.com>
2025-05-30 13:36:51 +0200

    comando aiuto

--- src/it/uniroma3/diadia/comandi/ComandoAiuto.java
+++ src/it/uniroma3/diadia/comandi/ComandoAiuto.java
@@ -3,24 +3,21 @@ package it.uniroma3.diadia.comandi;
 import it.uniroma3.diadia.IO;
 import it.uniroma3.diadia.Partita;
 
-public class ComandoAiuto implements Comando{
+public class ComandoAiuto extends AbstractComando{
 	static final public String[] ELENCO_COMANDI = {"vai", "aiuto", "fine","prendi", "posa", "guarda"};
-	private IO io;
+	
 	private final static String NOME = "aiuto";
 
 	@Override
 	public void esegui(Partita partita) {
 		
-		 if (this.io == null) {
-		        System.out.println("Errore: IO non è stato inizializzato!");
-		        return;
-		    }
+		 
 		for(int i=0; i< ELENCO_COMANDI.length; i++) 
-			io.mostraMessaggio(ELENCO_COMANDI[i]+" ");
-		io.mostraMessaggio("");
+			this.getIo().mostraMessaggio(ELENCO_COMANDI[i]+" ");
+		this.getIo().mostraMessaggio("");
 	}
 
-	@Override
+	/*@Override
 	public void setParametro(String parametro) {
 		
 
@@ -36,7 +33,7 @@ public class ComandoAiuto implements Comando{
 	public void setIo(IO io) {
 		this.io = io;
 	}
-	
+	*/
 	@Override
 	public String getNome() {
 		return NOME;
