43aa97ab5c0d0e2c70fdb3f656c25347d569ba5b
samuuu17 <samuelemiele0@gmail.com>
2025-05-30 14:05:40 +0200

    mi ero scordato di aggiornare fabbrica di comandi

--- src/it/uniroma3/diadia/comandi/FabbricaDiComandiFisarmonica.java
+++ src/it/uniroma3/diadia/comandi/FabbricaDiComandiFisarmonica.java
@@ -16,23 +16,24 @@ public Comando costruisciComando(String istruzione) {
 	String parametro = null;
 	Comando comando = null;
 	if (scannerDiParole.hasNext())
-	nomeComando = scannerDiParole.next(); // prima parola: nome del comando
+		nomeComando = scannerDiParole.next(); // prima parola: nome del comando
 	if (scannerDiParole.hasNext())
-	parametro = scannerDiParole.next(); // seconda parola: eventuale parametro
+		parametro = scannerDiParole.next();
+	// seconda parola: eventuale parametro
 	if (nomeComando == null)
-	comando = new ComandoNonValido();
+		comando = new ComandoNonValido();
 	else if (nomeComando.equals("vai"))
-	comando = new ComandoVai();
+		comando = new ComandoVai();
 	else if (nomeComando.equals("prendi"))
-	comando = new ComandoPrendi();
+		comando = new ComandoPrendi();
 	else if (nomeComando.equals("posa"))
-	comando = new ComandoPosa();
+		comando = new ComandoPosa();
 	else if (nomeComando.equals("aiuto"))
-	comando = new ComandoAiuto();
+		comando = new ComandoAiuto();
 	else if (nomeComando.equals("fine"))
-	comando = new ComandoFine();
+		comando = new ComandoFine();
 	else if (nomeComando.equals("guarda"))
-	comando = new ComandoGuarda();
+		comando = new ComandoGuarda();
 	else comando = new ComandoNonValido();
 	comando.setParametro(parametro);
 	comando.setIo(this.io);
